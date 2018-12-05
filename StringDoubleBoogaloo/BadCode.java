e522e003eb7099bc07872029eb4785858b0688e6
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2018-12-04 20:16:22 -0600

    Update BadCode.java

--- StringDoubleBoogaloo/BadCode.java
+++ StringDoubleBoogaloo/BadCode.java
@@ -1,3 +1,4 @@
 String s = "";  // not counted
+// 189 chars
 String a="";int i,n=0;for(i=0;i<s.length();i++)a+=Long.toString(s.charAt(i),2);for(i=0;i<a.length();i++)n+=Long.valueOf(a.charAt(i)+"");double r=n/2;do r=(r+n/r)/2;while(r*r>n+1e-9);r=1/r;
 System.out.println(a + "\n" + n + "\n" + r);  // not counted
