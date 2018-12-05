efe3b3d5f02f99d9a3f6311872ab2aa15c298a1e
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2018-12-05 17:56:22 -0600

    Update BadCode.py

--- StringDoubleBoogaloo/BadCode.py
+++ StringDoubleBoogaloo/BadCode.py
@@ -1,8 +1,8 @@
 s = 'Hey there epic fortnite gamers!'  # input("String? ")  # not counted
 
-# 84 chars
+# 83 chars
 b=0
-for x in bin(int.from_bytes(s.encode(),'big')).replace('0b',''):b+=int(x)
+for x in bin(int.from_bytes(s.encode(),'big')).replace('b',''):b+=int(x)
 b**-.5
 
 print(b**-.5)  # not counted
