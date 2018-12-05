1310e94026908e79aa0049d04d0bbc70e3f9e19c
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2018-12-04 20:10:32 -0600

    Create GoodCode.java

---
+++ StringDoubleBoogaloo/GoodCode.java
@@ -0,0 +1,18 @@
+String s = "";  // not counted
+String a = "";
+int i, n = 0;
+		
+for(i = 0; i < s.length(); i++)
+  a += Long.toString(s.charAt(i), 2);
+		
+for(i = 0; i < a.length(); i++)
+  n += Long.valueOf(a.charAt(i) + "");
+		
+double r = n / 2;
+		
+do r = (r + (n / r)) / 2;
+while (r * r > n + 1e-9);
+		
+r = 1 / r;
+		
+System.out.println(a + "\n" + n + "\n" + r + "\n");  // not counted
