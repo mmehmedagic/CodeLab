de03644039c462d78cc921d3d0cf6edd9bcc4810
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2018-12-05 17:58:43 -0600

    Update GoodCode.py

--- StringDoubleBoogaloo/GoodCode.py
+++ StringDoubleBoogaloo/GoodCode.py
@@ -1,7 +1,7 @@
 s = 'Hey there epic fortnite gamers!'  # input("String? ")  # not counted
 
 b = 0
-a = bin(int.from_bytes(s.encode(),'big')).replace('0b','')
+a = bin(int.from_bytes(s.encode(),'big')).replace('b','')
 for x in a:
   b += int(x)
 b**-.5
