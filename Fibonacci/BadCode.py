b60c515aa44cb2d0149e71a2462b4dd450e5ba93
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2019-10-23 15:57:20 -0500

    Combine two lines
    Initialization and initial addition combined into one line.

--- Fibonacci/BadCode.py
+++ Fibonacci/BadCode.py
@@ -1,12 +1,12 @@
 """
 21/10/2019:
 Given input integer n, calculate the Fibonacci sequence up to index n, lists 0 indexes of course.
--------
+--------
 Bad Code (actual entry)
+54 characters
 """
 n = int(input("Index? "))  # Not counted
-l=[]  # Counted
-l+=[0]  # Counted
+l=[0]  # Counted
 if n>1:l+=[1]  # Counted
 for x in range(n-2):l+=[l[x]+l[x+1]]  # Counted
 print(l)  # Not counted
