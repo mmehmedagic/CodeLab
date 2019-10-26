743d2ad33c7e8bd94ec54d1cad0690f65b94a445
mmehmedagic <35238246+mmehmedagic@users.noreply.github.com>
2019-10-26 15:09:04 -0500

    Sync logic with BadCode.py

--- Fibonacci/GoodCode.py
+++ Fibonacci/GoodCode.py
@@ -7,11 +7,11 @@ Good Code (readable)
 
 def fibonacci(num, lst=[]):
     lst.append(0)
-    if num > 1:
+    if num > 0:
         lst.append(1)
-    for x in range(num - 2):
+    for x in range(num - 1):
         lst.append(lst[x] + lst[x + 1])
-    print(lst)
+    print(lst[-1])
 
 
 fibonacci(10)
